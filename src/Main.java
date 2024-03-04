4b655ee0b88cb5dc77ecb599c14008f440314460
kghent4 <130794676+kghent4@users.noreply.github.com>
2024-03-04 11:59:34 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.ArrayList;
+
+public class Main {
+
+    // Variable(s)
+    public static ArrayList<Shape> shapes = new ArrayList<Shape>();
+
+    public static void main(String[] args) {
+
+        // Add shapes to list
+        Shape pie = new Circle("pie", 5); // argument is radius
+        Shape mint = new Circle("mint", 1);
+
+        Shape pizzaSlice = new Triangle("pizza slice", 2, 3, 4, 2, 3); // arguments are sideLen1, sideLen2, sideLen3
+        Shape cheeseCake = new Triangle("cheese cake", 2, 3, 4, 2, 3);
+
+        Shape candyBar = new Rectangle("candy bar", 3, 1); // arguments are width, height
+        Shape brownie = new Rectangle("brownie", 5, 5);
+
+        shapes.add(pie);
+        shapes.add(mint);
+        shapes.add(pizzaSlice);
+        shapes.add(cheeseCake);
+        shapes.add(candyBar);
+        shapes.add(brownie);
+
+        System.out.println("\nLearning About Shapes!\n");
+
+        for (Shape s : shapes) {
+            System.out.println(s + "\n");
+        }
+
     }
 }
