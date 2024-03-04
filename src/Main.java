92fa380ef59d5a61e39fd7670b1e560da5e3b2d0
kghent4 <130794676+kghent4@users.noreply.github.com>
2024-03-04 12:08:40 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -8,13 +8,13 @@ public class Main {
     public static void main(String[] args) {
 
         // Add shapes to list
-        Shape pie = new Circle("pie", 5); // argument is radius
+        Shape pie = new Circle("pie", 5); // arguments are item name, radius
         Shape mint = new Circle("mint", 1);
 
-        Shape pizzaSlice = new Triangle("pizza slice", 2, 3, 4, 2, 3); // arguments are sideLen1, sideLen2, sideLen3
+        Shape pizzaSlice = new Triangle("pizza slice", 2, 3, 4, 2, 3); // arguments are item name, sideLen1, sideLen2, sideLen3, base, height
         Shape cheeseCake = new Triangle("cheese cake", 2, 3, 4, 2, 3);
 
-        Shape candyBar = new Rectangle("candy bar", 3, 1); // arguments are width, height
+        Shape candyBar = new Rectangle("candy bar", 3, 1); // arguments are item name, width, height
         Shape brownie = new Rectangle("brownie", 5, 5);
 
         shapes.add(pie);
